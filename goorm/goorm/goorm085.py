c1a387a754751464e3657c44586bab450832784a
baeian <ddoory1103@gmail.com>
2022-10-04 10:10:18 +0900

    goorm085 edited

--- goorm/goorm/goorm085.py
+++ goorm/goorm/goorm085.py
@@ -1,21 +1,21 @@
-name = "마린"
-hp = 40
-damage = 5
-
-print("\n{} 유닛이 생성되었습니다.".format(name))
-print("체력 {0}, 공격력 {1}".format(hp,damage))
-
-tank_name="탱크"
-tank_hp=150
-tank_damage=35
-
-print("\n{} 유닛이 생성되었습니다.".format(tank_name))
-print("체력 {0}, 공격력 {1}".format(tank_hp,tank_damage))
-
+class Unit:
+   def __init__(self, name, hp,damage):
+      self.name = name
+      self.hp = hp
+      self.damage=damage
+      print("{} 유닛이 생성되었습니다.".format(self.name))
+      print("체력 {0}, 공격력 {1}\n".format(self.hp,self.damage))
+ 
 def attack(name,location,damage):
    print("\n{0} : {1} 방량으로 적군을 공격합니다. 공격력 {2}"
          .format(name,location,damage))
 
-attack(name,"1시",damage)
-attack(tank_name,"1시",tank_damage)
+marine1=Unit("마린",40,5)
+marine2=Unit("마린",40,5)
+tank=Unit("탱크",150,35)
+
+attack(marine1.name,"1시",marine1.damage)
+attack(marine2.name,"1시",marine2.damage)
+attack(tank.name,"1시",tank.damage)
+
 print()
