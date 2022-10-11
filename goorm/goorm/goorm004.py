14953d9eeaddbab1d6a263d2a6f554718a6c86e8
baeian <ddoory1103@gmail.com>
2022-10-11 09:58:38 +0900

    goorm005

--- goorm/goorm/goorm004.py
+++ goorm/goorm/goorm004.py
@@ -1,6 +1,9 @@
 print("Hello" + "World") #문자열 +연산
 print("Hello" * 3) #문자열 *연산
 
+print("Hello"[0]) #H
+print("Hello"[-1]) #o 인덱스!
+
 print('Hello') #Hello
 print("Hello") #Hello
 print("Hello 'world'") #Hello 'world'
---
+++ goorm/goorm/goorm005.py
@@ -0,0 +1,6 @@
+print('hello world'.capitalize())
+print('hello world'.upper())
+print('HELLO WORld'.lower())
+print('hello world'.__len__())
+print(len('hello world'))
+print('Hello world'.replace('world', 'programming'))
