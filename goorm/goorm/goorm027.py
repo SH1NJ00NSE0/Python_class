4af044bc4e550c9ac0d2e2f25764def19587d386
baeian <ddoory1103@gmail.com>
2022-10-11 10:13:52 +0900

    goorm027

---
+++ goorm/goorm/goorm027.py
@@ -0,0 +1,13 @@
+# print("Hello world 0")
+# print("Hello world 9")
+# print("Hello world 18")
+# print("Hello world 27")
+# print("Hello world 36")
+# print("Hello world 45")
+# print("Hello world 54")
+# print("Hello world 63")
+# print("Hello world 72")
+# print("Hello world 81")
+
+for i in range(0,82,9):
+   print("Hello world {0}".format(i))
